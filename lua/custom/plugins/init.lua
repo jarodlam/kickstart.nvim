7a5b68bc5460223738f7472a9ea2360f0688fcc8
Jarod Lam <10664255+jarodlam@users.noreply.github.com>
2025-09-30 13:21:34 +1000

    flash

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -66,6 +66,65 @@ return {
     'dstein64/nvim-scrollview',
   },
   {
+    'folke/flash.nvim',
+    event = 'VeryLazy',
+    opts = {
+      modes = {
+        -- options used when flash is activated through
+        -- a regular search with `/` or `?`
+        search = {
+          -- when `true`, flash will be activated during regular search by default.
+          -- You can always toggle when searching with `require("flash").toggle()`
+          enabled = true,
+        },
+      },
+    },
+    keys = {
+      -- {
+      --   's',
+      --   mode = { 'n', 'x', 'o' },
+      --   function()
+      --     require('flash').jump()
+      --   end,
+      --   desc = 'Flash',
+      --   remap = true,
+      -- },
+      {
+        'S',
+        mode = { 'n', 'x', 'o' },
+        function()
+          require('flash').treesitter()
+        end,
+        desc = 'Flash Treesitter',
+        remap = true,
+      },
+      -- {
+      --   'r',
+      --   mode = 'o',
+      --   function()
+      --     require('flash').remote()
+      --   end,
+      --   desc = 'Remote Flash',
+      -- },
+      -- {
+      --   'R',
+      --   mode = { 'o', 'x' },
+      --   function()
+      --     require('flash').treesitter_search()
+      --   end,
+      --   desc = 'Treesitter Search',
+      -- },
+      {
+        '<c-s>',
+        mode = { 'c' },
+        function()
+          require('flash').toggle()
+        end,
+        desc = 'Toggle Flash Search',
+      },
+    },
+  },
+  {
     'echasnovski/mini.files',
     opts = {
       windows = {
